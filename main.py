8e19f5d3964f871802a5a6f2a9b036f0ff2f1342
JoshuaMendez <joshuamendez0501@gmail.com>
2023-05-04 18:49:04 -0500

    2da versión

--- main.py
+++ main.py
@@ -4,8 +4,26 @@ ventana = ctk.CTk()
 #configuracion de la ventana
 ventana._set_appearance_mode("dark")#cambia el modo de la apariencia
 ventana.title("GARRAS, PERROS Y LLAVEROS S.A.")#asigna titulo a la ventana
-ventana.geometry("800x500+300+100")#dimenciones de la ventana
+ventana.geometry("1080x1080+300+100")#dimenciones de la ventana
+letra_elegida=""
+def funcion_aceptar():
+    global letra_elegida,letra_seleccionada,entrada_letranueva
+    letra_elegida=entrada_letranueva.get()
+    letra_seleccionada.configure(text="Letra seleccionada: "+str(letra_elegida))
+def cambiarletra():
+    global letra_elegida, entrada_letranueva, aceptar, letra_seleccionada, cambiarletraboton
+    #interfaz cambiar letra
+    Ingrese_letranueva=ctk.CTkLabel(ventana, text="Ingrese la letra:  ", font=("consolas",18,"bold"),width=400 , height=10)
+    Ingrese_letranueva.place(x=400,y=400)
+    entrada_letranueva=ctk.CTkEntry(ventana,font=("consolas",12,"bold"),width=100 , height=50)
+    entrada_letranueva.place(x=500,y=450)
+    aceptar = ctk.CTkButton(ventana, text="Aceptar", font=("arial",18),height=50,width=200,command=funcion_aceptar)
+    aceptar.place(x=700,y=450)
+    cambiarletraboton.place_forget()
+    
+
 def algoritmo_sugerencia():
+    global letra_elegida, letra_seleccionada, cambiarletraboton, imprimir
     texto=entrada_texto.get()
     texto=texto.upper()
     cantidad_repeticionL={"A":0,"B":0,"C":0,"D":0,"E":0,"F":0,"G":0,"H":0,"I":0,"J":0,"K":0,"L":0,"M":0,"N":0,"O":0,"P":0,"Q":0,"R":0,"S":0,"T":0,"U":0,"V":0,"W":0,"X":0,"Y":0,"Z":0 }
@@ -23,29 +41,24 @@ def algoritmo_sugerencia():
     resultado.configure(text=r)
     letra_elegida=letra_masrepetida
     letra_seleccionada=ctk.CTkLabel(ventana, text="Letra seleccionada: "+str(letra_elegida) , font=("consolas",20,"bold"))
-    letra_seleccionada.pack()
-    cambiarletra= ctk.CTkButton(ventana)
-    cambiarletra.pack()
-    imprimir= ctk.CTkButton(ventana)
-    imprimir.pack()
+    letra_seleccionada.place(x=500,y=300)
+    cambiarletraboton= ctk.CTkButton(ventana,text="Cambiar letra", font=("arial",18),height=50,width=200,command=cambiarletra)
+    cambiarletraboton.place(x=500,y=340)
+    imprimir= ctk.CTkButton(ventana,text="Imprimir", font=("arial",18),height=50,width=200)
+    imprimir.place(x=800,y=340)
+    
 
-
-
-#elementos de la ventana
-Titulo=ctk.CTkLabel(ventana, text="GARRAS, PERROS Y LLAVEROS S.A.", font=("consolas",20,"bold"))
-Titulo.pack()
-cuadro1= ctk.CTkFrame(ventana)
-cuadro1.pack()
-Ingrese_texto=ctk.CTkLabel(cuadro1, text="Ingrese las palabras , frases o poemas de su preferencia: ", font=("consolas",20,"bold"))
-Ingrese_texto.pack()
-entrada_texto=ctk.CTkEntry(cuadro1,font=("consolas",20,"bold"),width=400 , height=100)
-entrada_texto.pack()
-cuadro2= ctk.CTkFrame(ventana)
-cuadro2.pack()
-resultado=ctk.CTkLabel(cuadro2, text="", font=("consolas",20,"bold"))
-Enviar = ctk.CTkButton(ventana, text="Enviar", font=("arial",18),command=algoritmo_sugerencia)
-Enviar.pack()
-resultado.pack()
+#elementos de la ventana principal
+Titulo=ctk.CTkLabel(ventana, text="GARRAS, PERROS Y LLAVEROS S.A.", font=("consolas",25,"bold"))
+Titulo.place(x=340,y=30)
+Ingrese_texto=ctk.CTkLabel(ventana, text="Ingrese las palabras , frases o poemas de su preferencia: ", font=("consolas",18,"bold"),width=400 , height=10)
+Ingrese_texto.place(x=500,y=100)
+entrada_texto=ctk.CTkEntry(ventana,font=("consolas",12,"bold"),width=550 , height=50)
+entrada_texto.place(x=500,y=150)
+resultado=ctk.CTkLabel(ventana, text="", font=("consolas",14,"bold"))
+Enviar = ctk.CTkButton(ventana, text="Enviar", font=("arial",18),command=algoritmo_sugerencia,height=50,width=200)
+Enviar.place(x=500,y=230)
+resultado.place(x=710,y=230)
 
 
 ventana.mainloop()
