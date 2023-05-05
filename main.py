50ce391b6bf1201e79c0cfbee29df0affe7de1f9
sebasizq <sebastiansaavedra_2005@hotmail.com>
2023-05-04 21:49:13 -0500

    Version 3

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def algoritmo_sugerencia():
     texto = entrada_texto.get()
     texto = texto.upper()
     cantidad_repeticionL = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "H": 0, "I": 0, "J": 0, "K": 0, "L": 0,
-                            "M": 0, "N": 0, "O": 0, "P": 0, "Q": 0, "R": 0, "S": 0, "T": 0, "U": 0, "V": 0, "W": 0, "X": 0, "Y": 0, "Z": 0}
+                            "M": 0, "N": 0,"Ñ":0, "O": 0, "P": 0, "Q": 0, "R": 0, "S": 0, "T": 0, "U": 0, "V": 0, "W": 0, "X": 0, "Y": 0, "Z": 0}
     i = 0
     while i < len(texto):
         if texto[i] in cantidad_repeticionL:
